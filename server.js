6a8a0207e4c0dd3069037c3389bab0268a4d42fd
Yilun Xiao <yilunxiao2@gmail.com>
2021-05-13 18:44:22 -0400

    changed path \ to /

--- server.js
+++ server.js
@@ -23,9 +23,9 @@ const notes = [
 // Routes
 
 // Basic route that sends the user first to the AJAX Page
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, '\\public\\index.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
 
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '\\public\\notes.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
 
 // Displays all notes on file
 app.get('/api/notes', (req, res) => res.json(notes));
